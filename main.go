17b4fd4af8e4e57cab9e55f8fc4920eccaca8504
grian32 <grianguy32@gmail.com>
2026-02-25 21:11:04 +0200

    feat: add basic nav bar, add tailwind

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseGlob("html/*.html"))
 
 func main() {
-	log.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.ExecuteTemplate(w, "index.html", nil)
+	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8080", nil)
 }
